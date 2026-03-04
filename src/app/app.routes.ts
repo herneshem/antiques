3d710352145dbd92ce33b35695486770f9678cbc
herneshem <ernestoami@hotmail.com>
2026-03-04 16:29:37 +0100

    040326_4

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,8 @@ import { DetailsComponent } from './componentes/details/details.component';
 import { ViewComponent } from './componentes/view/view.component';
 
 export const routes: Routes = [
-  { path: 'view', component: ViewComponent },
-  { path: '', component: DetailsComponent },
-  { path: 'cart', component: CartComponent }
+ { path: '', component: ViewComponent },   // Página inicial
+  { path: 'details', component: DetailsComponent },
+  { path: 'cart', component: CartComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
